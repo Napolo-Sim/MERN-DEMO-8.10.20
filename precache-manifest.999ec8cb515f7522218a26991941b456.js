909c5cbb2484f666b1df93a45042f5b655ddaf42
Napoleon Vuong <napolo101@gmail.com>
2020-08-10 19:30:04 -0700

    Updates

---
+++ precache-manifest.999ec8cb515f7522218a26991941b456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7624829dcfcfe5fb3ad315e83c35f6d",
+    "url": "/MERN-DEMO-8.10.20/index.html"
+  },
+  {
+    "revision": "c8b407a23948651455b3",
+    "url": "/MERN-DEMO-8.10.20/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc03aed87d75d818f89c",
+    "url": "/MERN-DEMO-8.10.20/static/js/2.09f1b02a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MERN-DEMO-8.10.20/static/js/2.09f1b02a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b407a23948651455b3",
+    "url": "/MERN-DEMO-8.10.20/static/js/main.1610e5b7.chunk.js"
+  },
+  {
+    "revision": "f6e2ce8721a5d438a407",
+    "url": "/MERN-DEMO-8.10.20/static/js/runtime-main.19046824.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MERN-DEMO-8.10.20/static/media/logo.5d5d9eef.svg"
+  }
+]);
