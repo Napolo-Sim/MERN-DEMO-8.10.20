bce360046445c89cb1592ace83c3d7124cbdc4f3
Napoleon Vuong <napolo101@gmail.com>
2020-08-12 18:31:28 -0700

    Completed Code with correct yoda translation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TranslationInput from "./components/TranslationInput";
+import TranslationList from "./components/TranslationList";
+import axios from "axios";
 
 function App() {
+  const [translation, setTranslation] = useState({
+    text: "",
+    translationList: [],
+  });
+
+  const editText = (e) => {
+    setTranslation({ ...translation, [e.target.name]: e.target.value });
+  };
+
+  const submitText = (e) => {
+    e.preventDefault();
+    translate(translation.text).then((res) => {
+      setTranslation({
+        ...translation,
+        translationList: [...translation.translationList, res],
+      });
+    });
+  };
+
+  const translate = (originalText) => {
+    return new Promise((resolve, reject) => {
+      axios({
+        method: "POST",
+        url: "https://yodish.p.rapidapi.com/yoda.json",
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "x-rapidapi-host": "yodish.p.rapidapi.com",
+          "x-rapidapi-key":
+            "16328b5293mshd8cafb20152cec1p10d8e6jsnc2db601cf5f4",
+          useQueryString: true,
+        },
+        params: {
+          text: originalText,
+        },
+        data: {},
+      })
+        .then((response) => {
+          resolve(response.data.contents.translated);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://quotes.rest/qod")
+      .then((res) => translate(res.data.contents.quotes[0].quote))
+      .then((yodaTranslated) => console.log(yodaTranslated))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello ?</h1>
+      <div className="container">
+        <div className="jumbotron rounded-0">
+          <TranslationInput submitText={submitText} editText={editText} />
+        </div>
+      </div>
+
+      <TranslationList translationList={translation.translationList} />
     </div>
   );
 }
---
+++ src/components/TranslationInput.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const TranslationInput = (props) => {
+    return (
+        <form onSubmit={props.submitText}>
+            <div className="form-group">
+                <input
+                    name="text"
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter Text"
+                    onChange={props.editText}
+                />
+                <div className="input-group-append">
+                    <button
+                        className="btn btn-outline-secondary"
+                        type="submit"
+                        id="button-addon2"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default TranslationInput
